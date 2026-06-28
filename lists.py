fd220787625cb56f816a84dd375814211f5271df
Adam57055 <adamtwam85@gmail.com>
2026-06-28 00:41:30 -0400

    Enhance comment on lists usage in data science

--- lists.py
+++ lists.py
@@ -1,1 +1,5 @@
-#Lists are essential to storing variables
+#Lists are essential to storing variables for many reasons, like in data science and machine learning for seeing trends in data
+
+colors = [blue, red, green]
+
+
